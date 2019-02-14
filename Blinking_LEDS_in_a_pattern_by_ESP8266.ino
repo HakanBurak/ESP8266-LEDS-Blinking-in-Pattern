8a64a87ce9d9b4343f77993b087ade40e21a85e9
HakanBurak <47629941+HakanBurak@users.noreply.github.com>
2019-02-14 17:23:23 +0300

    Update Blinking_LEDS_in_a_pattern_by_ESP8266.ino

--- Blinking_LEDS_in_a_pattern_by_ESP8266.ino
+++ Blinking_LEDS_in_a_pattern_by_ESP8266.ino
@@ -1,13 +1,10 @@
 
-void setup() {
+void setup() { // Setup codes needed for recognizing your pin connections.      
   pinMode(0, OUTPUT);   
   pinMode(13, OUTPUT);
 }
-
-
-void loop() {
+void loop() {   // In loop all codes you wrote will be executed always in a loop. :)
   digitalWrite(0, LOW);  
- 
   digitalWrite(13, HIGH);
 delay(1000);
   digitalWrite(0, HIGH); 
